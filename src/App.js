afcdc108e8cfba5dc46b2a9f7465214e063ddf60
stutimunshi <64228226+stutimunshi@users.noreply.github.com>
2021-01-10 17:02:04 -0500

    Meal prep

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { useEffect, useState} from "react";
 import Food from "./Food"
 import './App.css';
-import style from './food.module.css';
 
 const App = () => {
   const API_ID = "1d2d9dca"
   const API_KEY = "af7a6021e072d1ac2b7463b132cf1efa";
-
+  // const API_KEY = "97ca8d43e4a249c08d36675136e074c0"
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('')
+  const [query, setQuery] = useState(``)
 
   useEffect(() => {
     getRecipe();
-  }, [query]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [query]); //eslint-disable-line 
 
   const getRecipe = async () => {
-
+    // const recipeName = e.target.elements.recipeName.value;
+    // e.preventDefault();
     const api_call = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${API_ID}&app_key=${API_KEY}`);
-
+    // const api_call = await fetch(`https://api.spoonacular.com/recipes/search?apiKey=${API_KEY}&number=1&query=${query}`);
+    // console.log(recipeName)
 
     const results = await api_call.json();
-
+    // this.setState({ recipes: results.recipes })
+    // console.log(this.state.recipes)
     setRecipes(results.hits);
     console.log(results.hits);
     
@@ -39,33 +41,19 @@ const App = () => {
   }
     return (
       <div className="App">
-          <img className= "logo" 
-            src="./logo.png" alt="logo"
-          />
+        <img class="banner" src="./logo.png" alt = ""/>
+        {/* <h1 className="App-title">Welcome to Recipe Roulette!</h1> */}
         <form onSubmit = {getSearch} className="search-form">
-          <input 
-            className="search-bar" 
-            type="text" placeholder = "Search Ingredient" 
-            value = {search} 
-            onChange ={searchUpdate}
-            placeholder="Type in your ingredients here..."
-          />
-          <button className="search-button" type="submit">
-            Search
-          </button>
+          <input className="search-bar" type="text" placeholder="Type in your ingredient here..." value = {search} onChange ={searchUpdate}/>
+          <button className="search-button" type="submit">Search</button>
         </form>
+        <header className="App-header">
         
-        <div className="recipes">
+        </header>
         {recipes.map(food => (
-          <Food 
-            title = {food.recipe.label} 
-            image = {food.recipe.image} 
-            ingredients = {food.recipe.ingredients} 
-            source = {food.recipe.url}
-          />
-
+          <Food title = {food.recipe.label} image = {food.recipe.image} ingredients = {food.recipe.ingredients} source = {food.recipe.url}/>
+          // <Food title = {Food.recipe.label} totalTime = {Food.recipe.totalTime} image = {Food.recipe.image} />
         ))}
-        </div>
       </div>
     );
 }
